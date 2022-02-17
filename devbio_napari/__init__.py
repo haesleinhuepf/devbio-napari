2e8b06ed3fccefd70a8cf30e6194670558a4c1d3
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-02-17 17:18:51 +0100

    ignore some not-installed dependencies

--- devbio_napari/__init__.py
+++ devbio_napari/__init__.py
@@ -5,16 +5,22 @@ from .__main__ import main
 
 from napari_tools_menu import register_dock_widget
 
-from RedLionfishDeconv.napari_plugin import RedLionfish_widget
-register_dock_widget( RedLionfish_widget, menu="Filtering / deconvolution > Richardson-Lucy (RedLionfish)")
+try:
+    from RedLionfishDeconv.napari_plugin import RedLionfish_widget
+    register_dock_widget( RedLionfish_widget, menu="Filtering / deconvolution > Richardson-Lucy (RedLionfish)")
+except ImportError:
+    pass
 
 from napari_3d_ortho_viewer._dock_widget import OrthoViewerWidget
 register_dock_widget( OrthoViewerWidget, menu="Visualization > Ortho Viewer (napari-3d-ortho-viewer)")
 
-from platymatch._dock_widget import DetectNuclei, EstimateTransform, EvaluateMetrics
-register_dock_widget(DetectNuclei, menu="Registration > Detect nuclei (platymatch)")
-register_dock_widget(EstimateTransform, menu="Registration > Estimate transform (platymatch)")
-register_dock_widget(EvaluateMetrics, menu="Registration > Evaluate metrics (platymatch)")
+try:
+    from platymatch._dock_widget import DetectNuclei, EstimateTransform, EvaluateMetrics
+    register_dock_widget(DetectNuclei, menu="Registration > Detect nuclei (platymatch)")
+    register_dock_widget(EstimateTransform, menu="Registration > Estimate transform (platymatch)")
+    register_dock_widget(EvaluateMetrics, menu="Registration > Evaluate metrics (platymatch)")
+except ImportError:
+    pass
 
 from napari_animation._qt import AnimationWidget
 register_dock_widget(AnimationWidget, menu="Visualization > Animation Wizard (napari-animation)")
